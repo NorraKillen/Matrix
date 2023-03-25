1f8b7c3ad2d4e3376c8a59061ff3bcd96c739669
Evgenii Maznyak <NorraKillen@yandex.ru>
2023-03-25 18:25:44 +0200

    exchange of diagonals

--- task1.py
+++ task1.py
@@ -4,9 +4,8 @@ a = []
 for i in range(n):
     a.append(input().split())
 for i in range(n):
-    for j in range(n):
 
-        a[i][i], a[ - i - 1][i] = a[ - i - 1][i], a[i][i]
+        a[i][i], a[n - i - 1][i] = a[n - i - 1][i], a[i][i]
 
 for i in range(n):
     print(*a[i])
